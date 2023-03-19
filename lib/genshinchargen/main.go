4671038081657484d553a8536caae950c3d5778b
6jarv91@gmail.com <6jarv91@gmail.com>
2023-03-19 17:59:57 +0100

    Genshinchargen updates

--- lib/genshinchargen/main.go
+++ lib/genshinchargen/main.go
@@ -18,7 +18,13 @@ var elements = NewWeightedSlice(map[string]int{
 	"Anemo":   100,
 	"Geo":     100,
 	"Dendro":  100,
+	"Abyss":   1,
 	"Omni":    1,
+})
+
+var rarity = NewWeightedSlice(map[string]int{
+	"5*": 10,
+	"4*": 10,
 })
 
 var models = NewWeightedSlice(map[string]int{
@@ -31,10 +37,13 @@ var models = NewWeightedSlice(map[string]int{
 })
 
 var scaling = NewWeightedSlice(map[string]int{
-	"ATK": 60,
-	"HP":  20,
-	"DEF": 10,
-	"EM":  10,
+	"ATK":             50,
+	"HP":              20,
+	"DEF":             10,
+	"EM":              10,
+	"Energy Recharge": 3,
+	"Healing Bonus":   2,
+	"Shield Strength": 1,
 })
 
 var roles = NewWeightedSlice(map[string]int{
@@ -47,12 +56,16 @@ var roles = NewWeightedSlice(map[string]int{
 })
 
 var strengths = NewWeightedSlice(map[string]int{
+	"has good AOE":                              10,
+	"has good elemental application":            10,
 	"it's a great battery":                      10,
 	"easy to build":                             10,
 	"has very high damage":                      10,
 	"has great vertical scaling":                10,
 	"has amazing animations and visual effects": 10,
+	"can snapshot buffs":                        8,
 	"can shred resistances":                     5,
+	"has damage resistance buffs":               2,
 	"can shred defense":                         2,
 })
 
@@ -61,9 +74,11 @@ var weaknesses = NewWeightedSlice(map[string]int{
 	"has energy issues":                           10,
 	"very hard to play":                           10,
 	"needs constellations to be good":             10,
-	"has scuffed ICDs":                            10,
 	"selfish and needs a lot of field time":       10,
+	"has scuffed ICDs":                            8,
 	"has very long cooldowns":                     8,
+	"has shitty multipliers":                      8,
+	"his kit is circlet impact":                   8,
 	"needs resistance to interruption to be good": 5,
 	"doesn't produce particles":                   2,
 	"can't crit":                                  2,
@@ -71,6 +86,7 @@ var weaknesses = NewWeightedSlice(map[string]int{
 
 type GeneratedCharacter struct {
 	name     string
+	rarity   string
 	element  string
 	model    string
 	scaling  string
@@ -80,8 +96,8 @@ type GeneratedCharacter struct {
 }
 
 func (c GeneratedCharacter) String() string {
-	return fmt.Sprintf("%s is a %s %s with a %s model and scales with %s, %s but %s.",
-		c.name, c.element, c.role, c.model, c.scaling, c.strength, c.weakness)
+	return fmt.Sprintf("%s is a %s %s %s with a %s model and scales with %s, %s but %s.",
+		c.name, c.rarity, c.element, c.role, c.model, c.scaling, c.strength, c.weakness)
 }
 
 func NewChar(name string) GeneratedCharacter {
@@ -90,6 +106,7 @@ func NewChar(name string) GeneratedCharacter {
 
 	rng := rand.New(rand.NewSource(generateSeed(name)))
 	result.element = elements.Random(rng)
+	result.rarity = rarity.Random(rng)
 	result.model = models.Random(rng)
 	result.scaling = scaling.Random(rng)
 	result.role = roles.Random(rng)

ad4f7592d4a656f888f0e6d226a4843e12c5877c
Jose Rojo <6jarv91@gmail.com>
2024-04-15 16:09:49 +0200

    Version change

--- cmd/jarvbot/commands.go
+++ cmd/jarvbot/commands.go
@@ -46,7 +46,7 @@ func onMessageCreated(ctx context.Context) func(ds *discordgo.Session, mc *disco
 // the command key must be lowercased
 var commands = map[string]command{
 	// public
-	"!version":                   simpleTextResponse("v3.5.1"),
+	"!version":                   simpleTextResponse("v3.5.2"),
 	"!source":                    simpleTextResponse("Source code: https://github.com/j4rv/discord-bot"),
 	"!genshindailycheckin":       answerGenshinDailyCheckIn,
 	"!genshindailycheckinstop":   answerGenshinDailyCheckInStop,

ce8117e28183a3f270003fe317e74d2e522040f8
Jarv <6jarv91@gmail.com>
2026-09-18 09:26:20 +0200

    Fix

--- cmd/jarvbot/images.go
+++ cmd/jarvbot/images.go
@@ -45,7 +45,7 @@ func answerQR(ds *discordgo.Session, mc *discordgo.MessageCreate, ctx context.Co
 }
 
 type deepFryQueryInput struct {
-	Noise      float64 `short:"n" long:"noise" default:"20" description:"Amount of image noise, 0-255""`
+	Noise      float64 `short:"n" long:"noise" default:"20" description:"Amount of image noise, 0-255"`
 	Saturation float64 `short:"s" long:"saturation" default:"3" description:"Saturation multiplier, 0-10"`
 	Quality    int     `short:"q" long:"quality" default:"2" description:"JPEG quality, 1-100"`
 	Iterations int     `short:"i" long:"iterations" default:"2" description:"Number of JPEG compression iterations, 1-10"`
